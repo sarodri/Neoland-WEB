575ccb7d810ca153686ded98556b4becdf54bff4
sarodri <sandra.ropero1987@gmail.com>
2024-03-26 18:07:41 +0100

    Changes of iteracion6 and new iteracion7

--- JAVASCRIPT/neoland-loops/iteracion6.js
+++ JAVASCRIPT/neoland-loops/iteracion6.js
@@ -8,13 +8,29 @@ const toys = [
     {id: 40, name: 'El gato felix'}
     ]
 
-for (let i =0; i<toys.length; i++){
-    for (const toy of toys) {
-        if (toy.name.includes('gato')) {
-            const x = toys.indexOf(toy);
-            toys.splice(x, 1);
-        }
+    // Con esta solución recorremos el array tantas veces como elemtos haya y hacemos el proceso más lento
+// for (let i =0; i<toys.length; i++){
+//     for (const toy of toys) {
+//         if (toy.name.includes('gato')) {
+//             const x = toys.indexOf(toy);
+//             toys.splice(x, 1);
+//         }
+//     }
+// }
+
+
+// Creo una variable para almacenar las posiciones de los elementos que contiene "gato" y con el for of solo recorremos el array una vez y comprobamos cada elemento, añadiendo al array juguetesEliminar los que contengas la palabra "gato"
+
+const juguetesEliminar = [];
+for (const toy of toys) {
+    if (toy.name.includes('gato')) {
+        juguetesEliminar.push(toy);
     }
+}
+// Eliminamos los juguetes del array padre a través de su posición
+for (const toy of juguetesEliminar) {
+    const i = toys.indexOf(toy);
+    toys.splice(i, 1);
 }
 
 console.log(toys)
