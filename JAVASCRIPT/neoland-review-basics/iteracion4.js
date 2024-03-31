c778c047e79581913d3eece37f221d9847bddde9
sarodri <sandra.ropero1987@gmail.com>
2024-03-31 10:32:18 +0200

    iteracion5

---
+++ JAVASCRIPT/neoland-review-basics/iteracion4.js
@@ -0,0 +1,19 @@
+// Crea una función llamada findArrayIndex que reciba como parametros un array de textos y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parametro. Haz varios ejemplos y compruebalos.
+const texto = "La silueta de un gato se movió vacilante a la luz de la luna y, al volver la cabeza para mirarlo, vi que no estaba solo: a unos quince metros de distancia, una figura había surgido de la sombra de la mansión de mi vecino y de pie, con las manos en los bolsillos, contemplaba la pimienta plateada de las estrellas.";
+
+const cadena = texto.split(" ")
+
+const arrayTexto = ['silueta','gato','luz','sombra', 'manos'];
+
+function findArrayIndex(arrayDeTextos, cadena) {
+  
+    
+}
+
+
+
+
+
+
+
+
